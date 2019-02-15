dd20713bff3cac3bbf6fb09e7ea8286a71300756
roy renno <royrenno@yahoo.com>
2019-02-15 23:06:31 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => {}
+      :hero_friends => [{}]
       #your key/value pairs here!
       },
     :capulet => {

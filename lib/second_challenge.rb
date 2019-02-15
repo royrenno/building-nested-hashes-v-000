da13459bef57c8f45b8ee4e600578ca832e8d3ae
roy renno <royrenno@yahoo.com>
2019-02-15 23:09:37 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -11,7 +11,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => [{}]
+      :heroine_friends => [{},{},{},[]]
 
       #your key/value pairs here!
       }}

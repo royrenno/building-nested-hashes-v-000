f9ee74713cb22210a6ece669a1e698024b97ed5f
roy renno <royrenno@yahoo.com>
2019-02-15 23:04:29 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch =>{},
-      :matriarch =>{},
-      :hero_friends =>{}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => {}
       #your key/value pairs here!
       },
     :capulet => {
